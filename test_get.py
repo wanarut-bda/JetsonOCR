2ec2635411cbd9ee9cc3d71c95eb56b09e2e3b1c
Wanarut <wanarut.b@bda.co.th>
2022-11-28 16:07:30 +0700

    add flow

--- test_get.py
+++ test_get.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np
 api_url = "http://localhost:1880/get_areas?device_id=OCR_0001"
 response = requests.get(api_url)
-print(response.json()['areas'])
+print(np.asarray(response.json()['areas'], dtype=int))
