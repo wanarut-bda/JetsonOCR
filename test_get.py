ad4ffae71f92154b9cd192c1ed12e11bef6efdef
Wanarut <wanarut.b@bda.co.th>
2022-11-28 18:57:23 +0700

    test local areas

--- test_get.py
+++ test_get.py
@@ -1,5 +1,17 @@
 import requests
 import numpy as np
+import yaml
+from yaml.loader import SafeLoader
+
 api_url = "http://localhost:1880/get_areas?device_id=OCR_0001"
-response = requests.get(api_url)
-print(np.asarray(response.json()['areas'], dtype=int))
+try:
+    response = requests.get(api_url)
+    areas_data = response.json()
+
+    with open('./areas.yaml', 'w') as file:
+        yaml.dump(areas_data, file)
+except :
+    with open('./areas.yaml') as f:
+        areas_data = yaml.load(f, Loader=SafeLoader)
+
+print(np.asarray(areas_data['areas'], dtype=int))
